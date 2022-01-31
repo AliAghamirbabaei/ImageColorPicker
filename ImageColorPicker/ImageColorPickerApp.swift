d407ed409e7675955f29f7ff1300b8eb0a940a7b
Ali Aghamirbabaei <Me@AliAghamirbabaei.com>
2022-02-01 02:09:30 +0330

    Initial Commit

---
+++ ImageColorPicker/ImageColorPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageColorPickerApp.swift
+//  ImageColorPicker
+//
+//  Created by Ali Aghamirbabaei on 2/1/22.
+//
+
+import SwiftUI
+
+@main
+struct ImageColorPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
