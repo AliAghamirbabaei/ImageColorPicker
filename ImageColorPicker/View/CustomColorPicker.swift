834a6f7b1db05dc86d6a5f510567a27b817953ff
Ali Aghamirbabaei <Me@AliAghamirbabaei.com>
2022-02-01 03:52:47 +0330

    Added PhotoPicker

--- ImageColorPicker/View/CustomColorPicker.swift
+++ ImageColorPicker/View/CustomColorPicker.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import PhotosUI
 
 // MARK: Making extension to call Image Color Picker
 extension View {
@@ -15,18 +16,133 @@ extension View {
             .fullScreenCover(isPresented: showPicker) {
                 
             } content: {
-                NavigationView{
-                    CustomColorPicker(color: color)
-                        .navigationTitle("Image Color Picker")
-                        .navigationBarTitleDisplayMode(.inline)
-                    // MARK: Close Button
-                        .toolbar {
-                            Button("Close"){
-                                showPicker.wrappedValue.toggle()
-                            }
+                Helper(showPicker: showPicker, color: color)
+            }
+    }
+}
+
+// MARK: Custom View for Color Picker
+struct Helper: View {
+    @Binding var showPicker: Bool
+    @Binding var color: Color
+    
+    // Image Picker Value
+    @State var showImagePicker: Bool = false
+    @State var imageData: Data = .init(count: 0)
+    
+    var body: some View {
+        NavigationView{
+            VStack(spacing: 10) {
+                // MARK: Image Picker View
+                GeometryReader { proxy in
+                    
+                    let size = proxy.size
+                    
+                    VStack(spacing: 12) {
+                        if let image = UIImage(data: imageData) {
+                            Image(uiImage: image)
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: size.width, height: size.height)
+                        }else {
+                            Image(systemName: "plus")
+                                .font(.system(size: 35))
+                            
+                            Text("Tap to add Image")
+                                .font(.system(size: 14, weight: .light))
                         }
+                    }
+                    .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
+                    .contentShape(Rectangle())
+                    .onTapGesture {
+                        // Show Image Picker
+                        showImagePicker.toggle()
+                    }
+                    
+                    ZStack(alignment: .top) {
+                        // Displaying the Selected Color
+                        Rectangle()
+                            .fill(color)
+                            .frame(height: 90)
+                        
+                        CustomColorPicker(color: $color)
+                            // MARK: Show Live Picker only
+                            .frame(width: 100, height: 50, alignment: .topLeading)
+                            .clipped()
+                            .offset(x: 15)
+                    }
                 }
             }
+            .navigationTitle("Image Color Picker")
+            .navigationBarTitleDisplayMode(.inline)
+            // MARK: Close Button
+            .toolbar {
+                Button("Close"){
+                    $showPicker.wrappedValue.toggle()
+                }
+            }
+            .sheet(isPresented: $showImagePicker) {
+                
+            } content: {
+                ImagePicker(showPicker: $showImagePicker, imageData: $imageData)
+            }
+        }
+    }
+}
+
+// MARK: Image Picker
+struct ImagePicker: UIViewControllerRepresentable {
+    
+    @Binding var showPicker: Bool
+    @Binding var imageData: Data
+    
+    func makeCoordinator() -> Coordinator {
+        return Coordinator(parent: self)
+    }
+    
+    func makeUIViewController(context: Context) -> PHPickerViewController {
+        var config = PHPickerConfiguration()
+        config.selectionLimit = 1
+        
+        let picker = PHPickerViewController(configuration: config)
+        picker.delegate = context.coordinator
+        
+        return picker
+    }
+    
+    func updateUIViewController(_ uiViewController: PHPickerViewController, context: Context) {
+        
+    }
+    
+    // Fetching Selected Image
+    class Coordinator: NSObject, PHPickerViewControllerDelegate {
+        var parent: ImagePicker
+        
+        init(parent: ImagePicker) {
+            self.parent = parent
+        }
+        
+        func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+            if let first = results.first {
+                first.itemProvider.loadObject(ofClass: UIImage.self) {[self] result, error in
+                    
+                    guard let err = error else { return }
+                    print("DEBUG: \(err)")
+                    
+                    guard let image = result as? UIImage else {
+                        parent.showPicker.toggle()
+                        return
+                    }
+                    
+                    parent.imageData = image.jpegData(compressionQuality: 1) ?? . init(count: 0)
+                    
+                    // Closing Picker
+                    parent.showPicker.toggle()
+                }
+            } else {
+                parent.showPicker.toggle()
+            }
+        }
     }
 }
 
@@ -48,6 +164,9 @@ struct CustomColorPicker: UIViewControllerRepresentable {
         
         // Connecting Delegate
         picker.delegate = context.coordinator
+        
+        // Removing Title
+        picker.title = ""
         
         return picker
     }
